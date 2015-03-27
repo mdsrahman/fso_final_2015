f1dfcd5ac3ce8ab401dcb58dc886d6bcfc0b83d8
mdsrahman <mdsrahman@cs.stonybrook.edu>
2015-03-27 00:16:45 -0400

    fgfdg

---
+++ march_15_final_fso_placement_algo/final_fso/src/test/test_target_handler.py
@@ -0,0 +1,37 @@
+import unittest
+from final_fso.src.target_handler import TargetHandler
+
+
+class TestGenerateInput(unittest.TestCase):
+  def setUp(self):
+    unittest.TestCase.setUp(self)
+    self.th = TargetHandler('./config.txt')
+    #self.gi.generateNodePositions()
+    print "Test for GenerateInput.."
+  
+
+  def test_heuristicTargetCover(self):
+    print "Set Covers:", sorted(self.th.node_cover)
+    print "Total Set Cover nodes:",len(self.th.node_cover)
+    print "Total Nodes:",self.th.number_of_nodes
+    print "Targets Covered:",self.th.no_of_targets_covered
+    print "Total Targets:",self.th.total_targets
+    print "Is Node-heap empty:",self.th.node_hp.isHeapEmpty()
+    
+    
+  
+    
+if __name__=='__main__':
+  unittest.main(verbosity = 2)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
