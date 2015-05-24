1089284dff79703209c7e8155c7931cc2707533a
mdsrahman <mdsrahman@cs.stonybrook.edu>
2015-05-24 17:32:18 -0400

    sdfsd

--- march_15_final_fso_placement_algo/final_fso/src/test/test_map_to_graph_generator.py
+++ march_15_final_fso_placement_algo/final_fso/src/test/test_map_to_graph_generator.py
@@ -3,10 +3,13 @@ from final_fso.src.map_to_graph_generator import MapToGraphGenerator
 
 class TestMapToGraphGenerator(unittest.TestCase):
   def setUp(self):
-    mapFilePath = './maps/nyc.osm'
-    outputFilePath = './maps/graphs/test.txt'
+    mapFilePath = './maps/small_nyc_test.osm'
+    outputFilePath = './maps/graphs/small_nyc_test_edges.txt'
+    short_edge_length = 100
+    long_edge_length = 1000
+    
     unittest.TestCase.setUp(self)
-    self.m2gg = MapToGraphGenerator(mapFilePath, outputFilePath)
+    self.m2gg = MapToGraphGenerator(mapFilePath, outputFilePath, short_edge_length, long_edge_length, False)
     print "Test for MapToGraphGenerator....."
     
 
@@ -16,6 +19,7 @@ class TestMapToGraphGenerator(unittest.TestCase):
     '''
     self.m2gg.generateMapToGraph()
     self.m2gg.debugGenerateVisualGraph()
+    self.m2gg.debugPrintSummary()
     
 if __name__=='__main__':
   unittest.main(verbosity = 2)
